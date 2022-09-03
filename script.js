711a7c66ac7e6d4fe645ad19e9edd6ae7bd5f674
satwikg3438 <102979033+satwikg3438@users.noreply.github.com>
2022-09-03 12:55:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function CC(){
+    document.body.style.backgroundColor=
+    'rgb(' + Math.round(Math.random() * 256) +
+    ',' +  Math.round(Math.random() * 254) +
+    ',' + Math.round(Math.random() * 255) + ')';
+}
+var audio=document.getElememtById("AuMu");
+function bell() {
+  audio.play;
+}
+function belly() {
+  CC();
+  bell();
+}
